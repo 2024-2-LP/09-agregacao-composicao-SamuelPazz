e4d23b67a05e9a3f147469d09b6f035f42a28d02
SamuelPazz <samuel.paz@sptech.school>
2024-10-23 19:49:02 -0300

    lista 9

--- src/main/java/school/sptech/Biblioteca.java
+++ src/main/java/school/sptech/Biblioteca.java
@@ -1,5 +1,76 @@
 package school.sptech;
 
-public class Biblioteca {
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
+public class Biblioteca {
+    private String nome;
+    private List<Livro> livros = new ArrayList<Livro>();
+
+    public Biblioteca() {
+    }
+
+    public Biblioteca(String nome, List<Livro> livros) {
+        this.nome = nome;
+        this.livros = livros;
+        this.livros = livros;
+    }
+
+    public void adicionarLivro(Livro livro) {
+        if (livro != null &&
+                livro.getTitulo() != null &&
+                !livro.getTitulo().isBlank() &&
+                livro.getAutor() != null &&
+                !livro.getAutor().isBlank() &&
+                livro.getDataPublicacao() != null
+        ) {
+            livros.add(livro);
+        }
+    }
+
+    public void removerLivroPorTitulo(String titulo) {
+        livros.removeIf(livro -> livro.getTitulo().equalsIgnoreCase(titulo));
+    }
+
+    public Livro buscarLivroPorTitulo(String titulo) {
+        return livros.stream()
+                .filter(livro -> livro.getTitulo().equalsIgnoreCase(titulo))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public Integer contarLivros() {
+        return this.livros.size();
+    }
+
+    public List<Livro> obterLivrosAteAno(Integer ano) {
+        return livros.stream()
+                .filter(livro -> livro.getDataPublicacao().getYear() <= ano)
+                .toList();
+    }
+
+    public List<Livro> retornarTopCincoLivros() {
+        return livros.stream()
+                .sorted(Comparator.comparingDouble(Livro::calcularMediaAvaliacoes).reversed())
+                .limit(5)
+                .toList();
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Biblioteca{" +
+                "livros=" + livros +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
 }
--- src/main/java/school/sptech/Livro.java
+++ src/main/java/school/sptech/Livro.java
@@ -1,5 +1,89 @@
 package school.sptech;
 
-public class Livro {
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Livro {
+    private String titulo;
+    private String autor;
+    private LocalDate dataPublicacao;
+    private List<Avaliacao> avaliacoes = new ArrayList<Avaliacao>();
+
+    public Livro() {
+    }
+
+    public Livro(String titulo, String autor, LocalDate dataPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.dataPublicacao = dataPublicacao;
+        this.avaliacoes = avaliacoes;
+
+    }
+
+
+    public void adicionarAvaliacao(String descricao, Double qtdEstrelas) {
+        if (descricao != null && qtdEstrelas != null) {
+            if (!descricao.isBlank() && qtdEstrelas >= 0 && qtdEstrelas <= 5) {
+                Avaliacao novaAvaliacao = new Avaliacao(descricao, qtdEstrelas);
+                avaliacoes.add(novaAvaliacao);
+            }
+        }
+    }
+
+
+public Double calcularMediaAvaliacoes() {
+    Double mediaAvaliacoes = 0.0;
+
+    if (avaliacoes.isEmpty())
+        return mediaAvaliacoes;
+
+    for (Avaliacao avaliacao : avaliacoes) {
+        mediaAvaliacoes += avaliacao.getQtdEstrelas();
+    }
+
+    return mediaAvaliacoes / avaliacoes.size();
+}
+
+public String getTitulo() {
+    return titulo;
+}
+
+public void setTitulo(String titulo) {
+    this.titulo = titulo;
+}
+
+public String getAutor() {
+    return autor;
+}
+
+public void setAutor(String autor) {
+    this.autor = autor;
+}
+
+public LocalDate getDataPublicacao() {
+    return dataPublicacao;
+}
+
+public void setDataPublicacao(LocalDate dataPublicacao) {
+    this.dataPublicacao = dataPublicacao;
+}
+
+public List<Avaliacao> getAvaliacoes() {
+    return avaliacoes;
+}
+
+public void setAvaliacoes(List<Avaliacao> avaliacoes) {
+    this.avaliacoes = avaliacoes;
+}
+
+@Override
+public String toString() {
+    return "Livro{" +
+            "titulo='" + titulo + '\'' +
+            ", autor='" + autor + '\'' +
+            ", dataPublicacao=" + dataPublicacao +
+            ", avaliacoes=" + avaliacoes +
+            '}';
+}
 }
